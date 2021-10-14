e4cb6104af6976cfd83f94550397a4a65a005066
Victor Cuches de León <69135983+VictorCuches@users.noreply.github.com>
2021-10-14 15:58:54 -0600

    Pruebas con jison

---
+++ proyecto2/main.js
@@ -0,0 +1,3 @@
+const parser = require('./src/Analizer/grammar')
+
+console.log(parser.parse("calcular 1+1*8;"))
--- proyecto2/src/index.js
+++ proyecto2/src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const parser = require('./Analizer/grammar')
+  //"jison": "jison .src/Analizer/Grammar.jison && mv Grammar.js ./src/Analizer/"
 
+
+  
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <div>Hola Mundo </div>, document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+var contenido 
+
+// EVENTOS
+
+const compilar = document.getElementById('compile');
+compilar.addEventListener('click', probando);
+
+var textEditor = document.getElementById("editor");
+
+
+function probando() {
+  console.log("Hola mundo")
+  console.log(parser.parse(contenido))
+   
+
+}
+
+function readFile(evento){ // lectura del archivo .json
+    
+  let archivo = evento.target.files[0];
+  if (archivo){
+      let reader = new FileReader();
+      reader.onload = function(e){
+          contenido = e.target.result;
+          console.log(contenido)
+          textEditor.textContent = contenido
+           
+                    
+          //generateElements(listaValores, tipoDato);
+
+      };
+      reader.readAsText(archivo); 
+
+  } else {
+      alert("No se ha seleccionado ningun archivo");
+  }
+}
+
+window.addEventListener('load', ()=>{ // cada vez que cambie 
+  document.getElementById('file').addEventListener('change',readFile)
+});
+
